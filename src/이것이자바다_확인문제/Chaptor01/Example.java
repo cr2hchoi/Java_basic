b6a93aa31c0d67b5c2d1a5bf3ea7c2aedb500475
cr2hChoi <20231046@sungshin.ac.kr>
2026-04-20 17:39:15 +0900

    이것이자바다 3판 1권 확인문제 풀이

---
+++ src/이것이자바다_확인문제/Chaptor01/Example.java
@@ -0,0 +1,9 @@
+package 이것이자바다_확인문제.Chaptor01;
+
+public class Example {
+    public static void main(String[] args) {
+        // 요구사항에 맞는 문자열 출력
+        System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
+    }
+}
+
