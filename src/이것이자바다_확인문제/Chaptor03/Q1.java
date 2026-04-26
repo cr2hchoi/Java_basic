e2f3d6385cfa12cc87f9f7670757a136f0dc9faa
choicr2h <20231046@sungshin.ac.kr>
2026-04-26 22:18:31 +0900

    이것이 자바다 확인문제 풀이

---
+++ src/이것이자바다_확인문제/Chaptor03/Q1.java
@@ -0,0 +1,11 @@
+
+package chaptor03;
+
+public class Q1 {
+    public static void main(String[] argss) {
+        int x = 10;
+        int y = 20;
+        int z = (++x) + (y--);
+        System.out.println(z);
+    }
+}
